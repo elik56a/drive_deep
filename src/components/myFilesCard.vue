bfa393b2da2b2d6abfbc020056fe14ece3b2d7dc
eliyahu <elik56a@gmail.com>
2020-01-01 10:41:54 +0200

    dvide app to pages and components

---
+++ src/components/myFilesCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+      <h1>
+         My Files Page
+      </h1>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'myFilesCard',
+
+  data: () => ({
+    items: []
+  }),
+  methods:{
+  },
+  created() {
+
+  }
+};
+</script>
